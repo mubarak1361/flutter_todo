3d711138c06886fd221c0e2dbc5954b6209d485d
Mubarak Mohideen <mubarak1361@gmail.com>
2018-04-13 16:38:20 +0530

    add sentry.io error tracking client

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/ui/todo_list.dart';
+import 'package:sentry/sentry.dart';
 
-void main() => runApp(new MyApp());
+final SentryClient sentry = new SentryClient(dsn: 'https://1180c9dd4e564620882fd3c926dc9a54:a229087c7ddf49aea473ac6d9d4573da@sentry.io/1188005');
+
+void main() {
+
+  FlutterError.onError = (errorDetails) async {
+    // errors caught by framework
+    await sentry.captureException(
+      exception: errorDetails.exception,
+      stackTrace: errorDetails.stack,
+    );
+  };
+
+  runApp(runZoned((){
+    return new MyApp();
+  },onError: (error,stackTrace) async {
+    // errors caught outside the framework
+    await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+  }));
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
